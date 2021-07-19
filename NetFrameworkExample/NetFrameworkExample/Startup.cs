7a0b58c14c69904690ad75d9a773120186e5733b
Bartłomiej Piepióra <bartek2560@gmail.com>
2021-07-19 21:36:52 +0200

    middleware

--- NetFrameworkExample/NetFrameworkExample/Startup.cs
+++ NetFrameworkExample/NetFrameworkExample/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.Owin;
 using Owin;
 
@@ -11,12 +13,25 @@ namespace NetFrameworkExample
     {
 	public void Configuration(IAppBuilder app)
 	{
-	    // New code:
-	 //   app.Run(context =>
+	    //New code:
+	 //   app.Use((context, next) =>
 	 //   {
-		//context.Response.ContentType = "text/plain";
-		//return context.Response.WriteAsync("Hello, world.");
+		//PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+		//return next.Invoke();
 	 //   });
+	    //   app.Run(context =>
+	    //   {
+	    //context.Response.ContentType = "text/plain";
+	    //return context.Response.WriteAsync("Hello, world.");
+	    //   });
+	}
+
+	private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string msg)
+	{
+	    var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+	    context.Get<TextWriter>("host.TraceOutput").WriteLine(
+		"Current IIS event: " + currentIntegratedpipelineStage
+		+ " Msg: " + msg);
 	}
     }
 }
