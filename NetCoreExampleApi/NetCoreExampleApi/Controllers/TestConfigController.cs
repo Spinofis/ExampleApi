62e4614daf7e35236a0d20d63aa0e386fedee2e4
Bartłomiej Piepióra <bartek2560@gmail.com>
2021-08-03 17:33:27 +0200

    get children

--- NetCoreExampleApi/NetCoreExampleApi/Controllers/TestConfigController.cs
+++ NetCoreExampleApi/NetCoreExampleApi/Controllers/TestConfigController.cs
@@ -53,6 +53,27 @@ namespace NetCoreExampleApi.Controllers
             return Ok(_configuration["test_env1"]);
         }
 
+	[HttpGet("config/get-children-test")]
+	public IActionResult GetChildrenTest() 
+	{
+	    string s = null;
+	    var section2 = _configuration.GetSection("section2");
+	    if (!section2.Exists()) 
+	    {
+		throw new InvalidOperationException("There is no such section section2!");
+	    }
+	    var children = section2.GetChildren();
+	    foreach (var subSection in children)
+	    {
+		int i = 0;
+		var key1 = subSection.Key + ":key" + i++.ToString();
+		var key2 = subSection.Key + ":key" + i.ToString();
+		s += key1 + " value: " + section2[key1] + "\n";
+		s += key2 + " value: " + section2[key2] + "\n";
+	    }
+	    return Ok(s);
+	}
+
         [HttpGet]
         public IEnumerable<string> Get()
         {
