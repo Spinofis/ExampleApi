4357169d8e283074eeeadd685c9b113081aecb9a
Bartłomiej Piepióra <bartek2560@gmail.com>
2021-09-11 12:18:46 +0200

    dodanie serilog do .net core

--- NetCoreExampleApi/NetCoreExampleApi/Program.cs
+++ NetCoreExampleApi/NetCoreExampleApi/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+
 
 namespace NetCoreExampleApi
 {
@@ -23,6 +25,10 @@ namespace NetCoreExampleApi
 		.ConfigureWebHostDefaults(webBuilder =>
 		{
 		    webBuilder.UseStartup<Startup>();
+		})
+		.UseSerilog((hostingContext, loggingConfiguration) => 
+		{
+		    loggingConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
 		});
     }
 }
